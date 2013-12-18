ff5cf0cf9682ffdbeb6e84de101981ef69d6ae46
Christophe Gragnic <christophegragnic@gmail.com>
2013-12-18 15:51:29 +0100

    Better comments in index.php.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ function dbg($var) {global $DEBUG, $$var; if ($DEBUG) echo ((isset($$var) ? ($va
 function req_URL() {return "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";}
 require('controller.php');
 
-// Parse the request and trigger the controller.
+// Parse the request in $method and $args.
 $req_URL = req_URL();
 if ($req_URL == $BASE) {
     $path = '';
@@ -29,6 +29,8 @@ if (count($expl) == 1) {
 }
 dbg('method');
 dbg('args');
+
+// Trigger the right method of the controller with the right args.
 $controller = new Controller();
 if (method_exists($controller, $method)) {
     call_user_func_array (array($controller, $method), $args);
