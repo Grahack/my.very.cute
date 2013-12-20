2c97471e43cedba6336cf1cf85b058a0cd7deb50
Christophe Gragnic <christophegragnic@gmail.com>
2013-12-20 21:27:41 +0100

    No French allowed, again.

--- controller.php
+++ controller.php
@@ -9,7 +9,7 @@ class Controller {
     public function index() {
         dbg("Home page");
     }
-    public function autre_methode($an_arg = "a great default value") {
+    public function what_is_the_url_of_this_method($an_arg = "a great default value") {
         dbg("another method, having an arg: ".$an_arg);
     }
 }
